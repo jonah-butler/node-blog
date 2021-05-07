fc9a051b31b2f028c7e48b74b8400b71c5fa874c
jonah-butler <jonahbutler6@gmail.com>
2021-05-07 17:23:39 -0400

    included local file containing about me object for the about me description page

---
+++ client/src/aboutMe.js
@@ -0,0 +1,9 @@
+export default {
+  jonah: {
+    name: 'Jonah Butler',
+    role: 'Developer',
+    location: 'Richmond, Va.',
+    description: 'Thanks for stopping by! My name\'s Jonah Butler. I\'m a developer with primary interests in both front-end and back-end with a preferred environment of Vue and Node.js. I\'m always posting here to share the bits of knowledge I have discovered along way or my new projects. When I\'m not writing code you can find me playing drums or rollerblading. If you want to talk, shoot me an email or find me online.',
+    img: 'me-compressed.jpg',
+  },
+};
