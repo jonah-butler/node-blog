2d65577e033d52628d12a000249855fccf6bf303
jonah-butler <jonahbutler6@gmail.com>
2021-05-07 17:22:42 -0400

    cleaned up file

--- server/app.js
+++ server/app.js
@@ -7,9 +7,6 @@ const express               = require('express'),
       mongoose              = require('mongoose'),
       methodOverride        = require('method-override'),
       morgan                = require('morgan'),
-      // passport              = require('passport'),
-      // passportLocalMongoose = require('passport-local-mongoose'),
-      // LocalStrategy         = require('passport-local'),
       blogRoutes            = require("./routes/blog.js"),
       userRoutes            = require('./routes/user.js'),
       sdkRoutes             = require('./routes/sdks3.js'),
@@ -30,77 +27,18 @@ try{
 } catch (error) {
   console.log(error);
 }
-// The name of the bucket that you have created
-// const BUCKET_NAME = 'dev-blog-resources';
-//
-// const s3 = new AWS.S3({
-//   accessKeyId: ID,
-//   secretAccessKey: SECRET
-// });
 
-// const params = {
-//  Bucket: BUCKET_NAME,
-//  ACL: 'public-read',
-//  ContentType: 'image/jpeg',
-//  Body: fs.createReadStream('./paint_store2.jpeg'),
-//  Key: "paint_store_new.jpeg",
-// };
-//
-// const params = {
-//  Bucket: BUCKET_NAME,
-//  ACL: 'public-read',
-//  ContentType: 'image/jpeg',
-//  Body: fs.createReadStream('./paint_store2.jpeg'),
-//  Key: "paint_store_new.jpeg",
-// };
-
-// s3.upload(params, (err, data) => {
-//   if(err){
-//     throw err;
-//   } else {
-//     console.log(data);
-//   }
-// })
-
-// s3.getObject(params, (err, data) => {
-//   if(err){
-//     throw err;
-//   } else {
-//     console.log(data);
-//     // console.log(data.Body.toString());
-//   }
-// })
-
-//DEPENDENCIES connect
-// app.options('*', (req, res) => {
-//   res.set('Access-Control-Allow-Origin', '*');
-//   res.send('ok');
-// });
-//
-// app.use((req, res) => {
-//   res.set('Access-Control-Allow-Origin', '*');
-// });
 
 app.use(function(req, res, next) {
  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  next();
 });
-// app.all('*', function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
+
 
 app.set("view engine", "ejs");
 app.use(cors());
 app.use(morgan("dev"));
-// app.use(cors({
-//   origin:['http://localhost:8080'],
-//   methods:['GET','POST'],
-//   credentials: true // enable set cookie
-// }));
 app.options('*', cors());
 app.use(express.static(__dirname + "/scripts"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -109,20 +47,6 @@ app.use(bodyParser.json({
 }));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
-
-// PASSPORT CONFIG
-// app.use(session({
-//   secret: 'much secret yet',
-//   resave: true,
-//   saveUninitialized: true,
-//   cookie: { secure: true }
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.use(new LocalStrategy(UserModel.authenticate()));
-// passport.use(UserModel.createStrategy());
-// passport.serializeUser(UserModel.serializeUser());
-// passport.deserializeUser(UserModel.deserializeUser());
 
 
 app.use("/", blogRoutes);
