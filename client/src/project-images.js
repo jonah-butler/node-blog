19dc734f97621093b96c6d2774ba3f36c28ee9da
jonah-butler <jonahbutler6@gmail.com>
2021-06-08 17:11:44 -0400

    removed local project images files and moved them to s3. storing urls here.

---
+++ client/src/project-images.js
@@ -0,0 +1,31 @@
+export default {
+  cn: [
+    'https://dev-blog-resources.s3.amazonaws.com/projects/cs-landing.jpg',
+  ],
+  mg: [
+    'https://dev-blog-resources.s3.amazonaws.com/projects/mg-landing.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/mg-landing2.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/mg-landing3.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/mg-landing4.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/mg-view.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/mg-view2.jpg',
+  ],
+  db: [
+    'https://dev-blog-resources.s3.amazonaws.com/projects/db-category.png',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/db-dashboard.png',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/db-ref.png',
+  ],
+  nb: [
+    'https://dev-blog-resources.s3.amazonaws.com/projects/nb1.png',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/nb2.png',
+  ],
+  rvalib: [
+    'https://dev-blog-resources.s3.amazonaws.com/projects/rva-bookology1.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/rva-teen2.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/rva-teen4.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/rva-teen5.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/rva-yava1.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/rva-yava2.jpg',
+    'https://dev-blog-resources.s3.amazonaws.com/projects/rva-yava3.jpg',
+  ],
+};
