441525d7887382ea35f357273905950a6abe922a
Jonah Butler <jonahbutler6@gmail.com>
2022-09-04 10:50:56 -0400

    updated about me technology list

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="container-full site-content">
     <UserCard :userObj="userObj" :localFilename="userObj.img"/>
-    <TechnologyBar technology="html" percent="100"/>
-    <TechnologyBar technology="css" percent="100"/>
     <TechnologyBar technology="JS/ES6" percent="100"/>
-    <TechnologyBar technology="vue" percent="90"/>
+    <TechnologyBar technology="vue2" percent="100"/>
+    <TechnologyBar technology="vue3" percent="100"/>
+    <TechnologyBar technology="vuex" percent="100"/>
     <TechnologyBar technology="node" percent="90"/>
+    <TechnologyBar technology="mobile dev: ionic" percent="90"/>
+    <TechnologyBar technology="aws s3" percent="90"/>
+    <TechnologyBar technology="aws sns" percent="90"/>
+    <TechnologyBar technology="aws lambda" percent="90"/>
+    <TechnologyBar technology="aws ec2" percent="90"/>
+    <TechnologyBar technology="aws cloudwatch" percent="90"/>
+    <TechnologyBar technology="aws iam" percent="90"/>
     <TechnologyBar technology="express" percent="90"/>
     <TechnologyBar technology="mvc architecture" percent="90"/>
     <TechnologyBar technology="rest design" percent="90"/>
+    <TechnologyBar technology="jest" percent="90"/>
     <TechnologyBar technology="express" percent="90"/>
-    <TechnologyBar technology="mongodb" percent="80"/>
+    <TechnologyBar technology="socket io" percent="90"/>
+    <TechnologyBar technology="mongodb" percent="90"/>
     <TechnologyBar technology="ui/ux" percent="80"/>
     <TechnologyBar technology="adobe suite" percent="80"/>
-    <TechnologyBar technology="ejs" percent="80"/>
-    <TechnologyBar technology="jquery" percent="80"/>
-    <TechnologyBar technology="aws" percent="70"/>
     <TechnologyBar technology="php" percent="70"/>
-    <TechnologyBar technology="vuex" percent="65"/>
+    <TechnologyBar technology="mysql" percent="70"/>
+    <TechnologyBar technology="postgres" percent="70"/>
+    <TechnologyBar technology="docker" percent="65"/>
     <TechnologyBar technology="data structures" percent="65"/>
-    <TechnologyBar technology="passport" percent="60"/>
+    <TechnologyBar technology="golang" percent="60"/>
+    <TechnologyBar technology="golang fiber" percent="60"/>
+    <TechnologyBar technology="golang gorm" percent="60"/>
+    <TechnologyBar technology="redis" percent="60"/>
     <TechnologyBar technology="sqlite" percent="50"/>
-    <TechnologyBar technology="babel" percent="40"/>
-    <TechnologyBar technology="jest" percent="40"/>
+    <TechnologyBar technology="c++" percent="10"/>
   </div>
 </template>
 
