eb44d7df339872e4478b2316d61a9a4983306e5f
Jonah Butler <jonahbutler6@gmail.com>
2023-01-23 22:07:02 -0500

    updating models for more flexibility

--- server/models/blog.js
+++ server/models/blog.js
@@ -1,69 +1,76 @@
-const mongoose          = require("mongoose"),
-      slugify           = require("slugify"),
-      marked            = require("marked"),
-      createDomPurify   = require("dompurify"),
-      { JSDOM }         = require("jsdom"),
-      domPurify         = createDomPurify(new JSDOM().window)
+const mongoose = require("mongoose"),
+  slugify = require("slugify"),
+  marked = require("marked"),
+  createDomPurify = require("dompurify"),
+  { JSDOM } = require("jsdom"),
+  domPurify = createDomPurify(new JSDOM().window);
 
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    featuredImageLocation: {
+      type: String,
+    },
+    featuredImageTag: {
+      type: String,
+    },
+    featuredImageKey: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+    sanitizedHTML: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: false,
+    },
+    categories: {
+      type: [String],
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    published: {
+      type: Boolean,
+      required: false,
+    },
+    publishedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const blogSchema = new mongoose.Schema({
-  title:{
-    type: String
-  },
-  slug:{
-    type: String,
-    required: true,
-    unique: true,
-  },
-  featuredImageLocation:{
-    type: String,
-  },
-  featuredImageTag:{
-    type: String,
-  },
-  featuredImageKey:{
-    type: String,
-  },
-  text:{
-    type: String
-  },
-  sanitizedHTML:{
-    type: String,
-    required: true
-  },
-  author:{
-    type: String,
-    required: false,
-    default: "Jonah"
-  },
-  categories:{
-    type: [String]
-  },
-  rating:{
-    type: Number,
-    default: 0
-  },
-  views:{
-    type: Number,
-    default: 0
-  },
-  published: {
-    type: Boolean,
-    required: false,
-  },
-}, {
-	timestamps: true,
-})
-
-blogSchema.pre('validate', function(next) {
-  if(this.title) {
-    this.slug = slugify(this.title, {lower: true, strict: true})
+blogSchema.pre("validate", function (next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
   }
 
-  if(this.text) {
+  if (this.text) {
     this.sanitizedHTML = domPurify.sanitize(marked(this.text));
   }
   next();
-})
+});
 
 module.exports = mongoose.model("BlogPost", blogSchema);
