f05d1bb5b4faf59d9f040bb3b65d49da448d183f
Jonah Butler <jonahbutler6@gmail.com>
2023-06-24 11:53:12 -0400

    will it work though?

--- client/src/components/inputs/Editorjs.vue
+++ client/src/components/inputs/Editorjs.vue
@@ -6,7 +6,8 @@
 </template>
 
 <script>
-import EditorJS from '@editorjs/editorjs';
+// import EditorJS from '@editorjs/editorjs';
+const EditorJS = required('@editorjs/editorjs')
 import Header from '@editorjs/header';
 import List from '@editorjs/list';
 import InlineCode from '@editorjs/inline-code';
