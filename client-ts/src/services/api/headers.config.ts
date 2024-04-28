585399d5893c5403b3c6bfbdab8c45ca1f62aef0
Jonah Butler <jonahbutler6@gmail.com>
2024-04-27 23:14:24 -0400

    do not need it

--- client-ts/src/services/api/headers.config.ts
+++ client-ts/src/services/api/headers.config.ts
@@ -1,9 +1,7 @@
 // this will come from pinia store eventually
-const DUMMY_TOKEN = "123kjs0f9123421kl3j4";
-
 const HEADERS = {
   headers: {
-    Authorization: `Bearer ${DUMMY_TOKEN}`,
+    Authorization: `Bearer `,
   },
 };
 
