d5edbdfe7ae0304877e7f2e29c1d08f624e24bf8
Jonah Butler <jonahbutler6@gmail.com>
2024-09-21 09:57:56 -0400

    reverted endpoint

--- client/src/services/api/api.config.ts
+++ client/src/services/api/api.config.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://jonahbutler-dev.herokuapp.com',
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://jonahbutler-dev.herokuapp.com',
+  // baseURL: 'http://localhost:4000',
 });
 
 export { api };
